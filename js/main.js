fa71258a68556208198152e8e01da9ad3fbc1df1
J. Cole Brand <jcolebrand@gmail.com>
2012-09-24 03:42:05 -0500

    committed changes make progress

---
+++ js/main.js
@@ -0,0 +1,32 @@
+(function($,currentBrowser){
+  $.getJSON('fakeapi/browsers.js',function(browsers){
+    var cookieValue = $.cookie('updateBrowser'),
+        versionOk = false,
+        badBrowser = false,
+        browser = browsers[currentBrowser.name];
+    
+    if (browser){
+      //we have an actual browser to work with
+      $('.updateBrowser'+currentBrowser.name+' a img').attr('src','img/update-btn.png');
+      $('#currentBrowserName').text(browser.display);
+      if (currentBrowser.versionNumber >= browser.minVersion){
+        versionOk = true;
+      }
+    } else {
+      $('#updateBrowserSupportedText').hide();
+      badBrowser = true;
+      //we have an unidentified browser
+    }
+    
+    if ((!versionOk && !cookieValue) || badBrowser){
+      $('#updateBrowser.hiddenOnLoad, #updateBrowserMask.hiddenOnLoad').removeClass('hiddenOnLoad');
+      $(document).on('click','#closeUpdateBrowser',function (evt){
+        var checked = $('#hideUpdateBrowser').attr('checked');
+        if (checked){
+          $.cookie('updateBrowser','udpateBrowser',{expires: 3650 /*ten years*/,path:'/'})
+        }
+        $('#updateBrowser, #updateBrowserMask').addClass('hiddenOnLoad');
+      });
+    }
+  });
+})(jQuery, $.browser)
