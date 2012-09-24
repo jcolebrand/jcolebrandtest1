0f21f4ebca0412d78e27a28b69bbbaf6825e2229
J. Cole Brand <jcolebrand@gmail.com>
2012-09-24 02:45:41 -0500

    testtesttest

--- js/updateBrowser.js
+++ js/updateBrowser.js
@@ -1,9 +1,10 @@
 (function($,currentBrowser){
+  console.log(currentBrowser);
   $.get('fakeapi/browsers.js',function(browsers){
     var cookieValue = $.cookie('updateBrowser'),
         versionOk = false,
         browser = browsers[currentBrowser.name];
-    
+    console.log(browser);
     if (browsers[currentBrowser.name]){
       //we have an actual browser to work with
       $('.updateBrowser'+currentBrowser.name+' a img').attr('src','img/download-btn.png');
